c924a018473af537d4da593454c79b3cbfc802d1
Abdullah Mustapha <a41622100@gmail.com>
2024-01-04 17:53:10 +0000

    Added js

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+// Get references to DOM elements
 const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
+// Event listener for the search button click
 search.addEventListener('click', () => {
 
+    // OpenWeatherMap API key and user-entered city
     const APIKey = '05f4ee67848cc2f2685db6e4c1cf5f3e';
     const city = document.querySelector('.search-box input').value;
 
+    // Check if the city input is empty
     if (city === '')
         return;
 
+    // Fetch weather data from the OpenWeatherMap API
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
 
+            // Handle the case when the city is not found
             if (json.cod === '404') {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
@@ -25,15 +31,18 @@ search.addEventListener('click', () => {
                 return;
             }
 
+            // Reset the error display and animation
             error404.style.display = 'none';
             error404.classList.remove('fadeIn');
 
+            // Get references to weather-related DOM elements
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
 
+            // Set weather icon based on weather condition
             switch (json.weather[0].main) {
                 case 'Clear':
                     image.src = 'images/clear.png';
@@ -59,19 +68,17 @@ search.addEventListener('click', () => {
                     image.src = '';
             }
 
+            // Display weather information
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
             wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
+            // Display weather information and set animation
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
             weatherBox.classList.add('fadeIn');
             weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
-
-
         });
-
-
 });
